4749db1889770592a54064111355234e49aefc7f
Ryan Kurniawan <ryansg41@gmail.com>
2022-05-12 22:38:56 +0700

    feat: import fontawesome

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPhone, faPlus, faMinus, faEquals } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(faPhone, faPlus, faMinus, faEquals);
 const app = createApp(App)
 
 app.use(router)
-
+app.component("font-awesome-icon", FontAwesomeIcon)
 app.mount('#app')
