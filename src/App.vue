8e72a8db952fb215e43ed83f2a27c410d371a6bb
Ryan Kurniawan <ryansg41@gmail.com>
2022-05-12 20:13:15 +0700

    feat: import work sans font family

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,8 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 <style>
 @import "@/assets/base.css";
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;300;500&display=swap");
+body {
+  font-family: "Work Sans", sans-serif;
+}
 </style>
